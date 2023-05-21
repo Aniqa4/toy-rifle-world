f0fbec95ee7b77ce4fcf3f71bfa629e3a23ffc53
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-05-21 23:52:31 +0600

    changed button color

--- src/Components/MyToys.jsx
+++ src/Components/MyToys.jsx
@@ -73,7 +73,7 @@ function MyToys() {
       <h1 className=' text-center text-2xl font-semibold my-20 py-5 bg-gray-100'>MY TOYS</h1>
       <div className="text-right mb-4">
         {!sortedByPrice ? (
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          <button className="bg-cyan-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={sortDataByPrice}
           >
             Sort by Price

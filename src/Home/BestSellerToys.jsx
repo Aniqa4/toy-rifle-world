924179847f734b6ce1545bd9158e602f135bf57e
Aniqa <aanika.chowdhury04@gmail.com>
2023-12-10 16:40:47 +0600

    changed css for home sections

--- src/Home/BestSellerToys.jsx
+++ src/Home/BestSellerToys.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function BestSellerToys() {
     return (
         <div className='my-10 mx-5'>
-            <h1 className=' text-center text-5xl mb-5 font-semibold'>Best Seller Toys</h1>
+            <h1 className=' text-center text-3xl mb-5 font-semibold uppercase'>Best Seller Toys</h1>
             <div className='md:grid grid-cols-3 gap-10 rounded'>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10 text-center'>
                     <img src="m416.jpg" className='w-11/12'/>
                     <h1 className=' font-semibold pt-5'>M416 Toy Assault Rifle</h1>
                 </div>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10 text-center'>
                     <img src="ak.jpg" />
                     <h1 className=' font-semibold pt-5'>AKM Toy Assault Rifle</h1>
                 </div>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10 text-center'>
                     <img src="dp28.jpeg" />
                     <h1 className=' font-semibold pt-5'>DP-28 Toy Light Machine Gun</h1>
                 </div>
--- src/Home/NewArivals.jsx
+++ src/Home/NewArivals.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 function NewArivals() {
     return (
         <div className='mx-5'>
-            <h1 className=' text-center text-5xl mb-5 font-semibold'>New Arrivals</h1>
+            <h1 className=' text-center text-3xl mb-5 font-semibold uppercase'>New Arrivals</h1>
             <div className='md:grid grid-cols-3 gap-10 rounded'>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10'>
                     <img src="famas.jpeg" />
-                    <h1 className=' font-semibold pt-5'>FAMAS Toy Assault Rifle</h1>
+                    <h1 className=' font-semibold pt-5 text-center'>FAMAS Toy Assault Rifle</h1>
                 </div>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10'>
                     <img src="mini14.jpg" />
-                    <h1 className=' font-semibold pt-5'>Mini14 Toy Semi-Assault Rifle</h1>
+                    <h1 className=' font-semibold pt-5 text-center'>Mini14 Toy Semi-Assault Rifle</h1>
                 </div>
-                <div className='bg-gray-100 p-5'>
+                <div className='bg-gray-100 p-10'>
                     <img src="m24.jpg" />
-                    <h1 className=' font-semibold pt-5'>M24 Toy Sniper Rifle</h1>
+                    <h1 className=' font-semibold pt-5 text-center'>M24 Toy Sniper Rifle</h1>
                 </div>
             </div>
         </div>
